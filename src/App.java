2ceb837e1133342baf642d426f655a0b2bace37f
eddmastr7 <eduardondsaguilera@gmail.com>
2024-08-01 23:54:33 -0600

    first commit

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+    class Test {
+    public static void main(String []args)
+    {
+        System.out.println("My First Java Program.");
+    }
+};
+}
